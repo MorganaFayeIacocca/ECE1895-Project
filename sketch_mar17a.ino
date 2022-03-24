4ff28a6f589cc492240a47775573ea087cf64de0
astra670 <102246316+astra670@users.noreply.github.com>
2022-03-24 12:24:33 -0400

    HelloWorld Arduino files
    The two blinking sequence arduino files

---
+++ sketch_mar17a.ino
@@ -0,0 +1,25 @@
+#define redLED 8
+#define pointLED 9
+int temp = LOW;
+
+void setup() {
+  pinMode(redLED, OUTPUT);    // sets the digital pin 14 as output
+  pinMode(pointLED, INPUT);     //sets the digital pin 15 as input
+}
+
+void loop() {
+  temp = digitalRead(pointLED);
+  if(temp == HIGH)
+  { 
+    digitalWrite(redLED, HIGH); // sets the digital pin 14 on
+    delay(40);            // waits for a second
+    digitalWrite(redLED, LOW);  // sets the digital pin 14 off
+    delay(40);  
+                // waits for a second
+  }
+  else ( temp == LOW);
+  {
+    digitalWrite(redLED, LOW);
+  }
+  temp = digitalRead(pointLED);
+}
